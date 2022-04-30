1b6e1091b66011d2719942f30c3efffba3239124
Robert de Bath <rdebath@tvisiontech.co.uk>
2022-04-30 19:22:06 +0100

    update

--- edlin.h
+++
@@ -1,2 +0,0 @@
-/* This file was automatically generated.  Do not edit! */
-#undef INTERFACE
--- utf8.h
+++
@@ -1,5 +0,0 @@
-/* This file was automatically generated.  Do not edit! */
-#undef INTERFACE
-int decodeutf8(int ch,int *utfstate);
-#define UTFNIL	(-257)
-#define INTERFACE 0
