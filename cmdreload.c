ee56a925e18a30af79eba9cf34cb9edd9013601e
Robert de Bath <rdebath@tvisiontech.co.uk>
2022-09-27 08:49:19 +0100

    update

---
+++ cmdreload.c
@@ -0,0 +1,33 @@
+#include <string.h>
+
+#include "cmdreload.h"
+
+/*HELP reload H_CMD
+&T/reload [all]
+&T/reload&S -- Reloads your session
+&T/reload all&S -- Reloads everybody on this level
+&T/reload classic&S -- Reloads your session in classic mode.
+*/
+#if INTERFACE
+#define CMD_RELOAD  {N"reload", &cmd_reload}
+#endif
+void
+cmd_reload(char * UNUSED(cmd), char * arg)
+{
+    if (arg == 0) {
+	if (classic_limit_blocks) {
+	    level_block_limit = client_block_limit;
+	    classic_limit_blocks = 0;
+	}
+	check_block_queue(1);
+	send_map_reload();
+    } else if (strcasecmp(arg, "classic") == 0 && extn_blockdefn && customblock_enabled) {
+	level_block_limit = Block_CP;
+	classic_limit_blocks = 1;
+	send_map_reload();
+    } else if (strcasecmp(arg, "all") == 0) {
+	level_block_queue->generation += 2;
+    } else
+	printf_chat("&WUsage: /reload [all]");
+    return;
+}
---
+++ permission.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "permission.h"
+
+int
+perm_level_check(char * levelname, int expand_plus)
+{
+    if (levelname == 0) {
+	if (expand_plus == 0) levelname = current_level_name;
+	else {
+	    printf_chat("&WPermission denied on current level");
+	    return 0;
+	}
+    }
+    char userlevel[256] = "";
+    if (server->allow_user_levels) {
+	saprintf(userlevel, "%s+", user_id);
+	if (expand_plus && strcmp(levelname, "+") == 0)
+	    strcpy(levelname, userlevel);
+	if (strcmp(levelname, userlevel) == 0)
+	    return 1;
+    }
+
+    // TODO: Levels with list of users who can access.
+
+    if (!client_trusted) {
+	if (*userlevel)
+	    printf_chat("&WPermission denied, your level name is '%s'", userlevel);
+	else if (levelname == current_level_name)
+	    printf_chat("&WYou do not have permission for level \"%s\"", levelname);
+	else
+	    printf_chat("&WPermission denied");
+        return 0;
+    }
+    return 1;
+}
