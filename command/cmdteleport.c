d06d464d9fc8ded86d0af60a8ede1092ace0360e
Robert de Bath <rdebath@tvisiontech.co.uk>
2023-10-25 07:49:59 +0100

    Add /ascend and /descend

--- command/cmdteleport.c
+++ command/cmdteleport.c
@@ -7,6 +7,8 @@
     {N"move", &cmd_tp, CMD_ALIAS}, \
     {N"teleport", &cmd_tp, CMD_ALIAS}, \
     {N"back", &cmd_back}, \
+    {N"ascend", &cmd_ascend}, \
+    {N"descend", &cmd_descend}, \
     {N"spawn", &cmd_spawn}
 #endif
 
@@ -31,6 +33,16 @@ Teleports you to the spawn location of the level.
 Takes you back to the position you were before the last teleportation.
 */
 
+/*HELP ascend H_CMD
+&T/Ascend
+Teleports you to the first free space above you.
+*/
+
+/*HELP descend H_CMD
+&T/Descend
+Teleports you to the first free space below you.
+*/
+
 xyzhv_t player_back_posn = {0};
 
 void
@@ -43,23 +55,6 @@ cmd_spawn(char * UNUSED(cmd), char *UNUSED(arg))
 
     send_posn_pkt(-1, &player_posn, level_prop->spawn);
     player_posn = level_prop->spawn;
-}
-
-void
-cmd_back(char * UNUSED(cmd), char *UNUSED(arg))
-{
-    if (!level_prop || (level_prop->hacks_flags & 8)) {
-	printf_chat("&cTeleport is currently disabled");
-	return;
-    }
-
-    if (!player_back_posn.valid) {
-	printf_chat("You have not teleported anywhere yet");
-	return;
-    }
-
-    send_posn_pkt(-1, &player_posn, player_back_posn);
-    player_posn = player_back_posn;
 }
 
 void
@@ -137,4 +132,110 @@ conv_ord(char * s, int ref, int ioff)
 
     double f = strtod(s, 0);
     return (int)(f*32) + ref + ioff;
+}
+
+void
+cmd_back(char * UNUSED(cmd), char *UNUSED(arg))
+{
+    if (!level_prop || (level_prop->hacks_flags & 8)) {
+	printf_chat("&cTeleport is currently disabled");
+	return;
+    }
+
+    if (!player_back_posn.valid) {
+	printf_chat("You have not teleported anywhere yet");
+	return;
+    }
+
+    send_posn_pkt(-1, &player_posn, player_back_posn);
+    player_posn = player_back_posn;
+}
+
+void
+cmd_ascend(char * UNUSED(cmd), char *UNUSED(arg))
+{
+    if (!level_prop || !level_blocks || (level_prop->hacks_flags & 8)) {
+	printf_chat("&c/Ascend is currently disabled");
+	return;
+    }
+
+    xyzhv_t newposn = player_posn;
+    int x,y,z;
+
+    x = player_posn.x/32; // check range [0..cells_x)
+    y = (player_posn.y+16)/32;// Add half a block for slabs
+    z = player_posn.z/32;
+    if (x<0 || z<0 || x >= level_prop->cells_x || z >= level_prop->cells_z) {
+	printf_chat("There are no blocks above to ascend to.");
+	return;
+    } else {
+	if (y<0) y = -1;
+	for(;;) {
+	    y++;
+	    if (y >= level_prop->cells_y) {
+		printf_chat("There are no blocks above to ascend to.");
+		return;
+	    }
+	    if (level_blocks[World_Pack(x, y, z)] != Block_Air)
+		break;
+	}
+	for(; y<level_prop->cells_y; y++) {
+	    uintptr_t index = World_Pack(x, y, z);
+	    if (level_blocks[index] == Block_Air &&
+		(y+1 == level_prop->cells_y ||
+		    level_blocks[World_Pack(x, y+1, z)] == Block_Air))
+		break;
+	}
+    }
+
+    newposn.y = y*32 + 16;
+    send_posn_pkt(-1, &player_posn, newposn);
+    player_posn = newposn;
+}
+
+void
+cmd_descend(char * UNUSED(cmd), char *UNUSED(arg))
+{
+    if (!level_prop || (level_prop->hacks_flags & 8)) {
+	printf_chat("&c/Descend is currently disabled");
+	return;
+    }
+
+    xyzhv_t newposn = player_posn;
+    int x,y,z;
+
+    x = player_posn.x/32; // check range [0..cells_x)
+    y = (player_posn.y+16)/32;// Add half a block for slabs
+    z = player_posn.z/32;
+    if (x<0 || z<0 || x >= level_prop->cells_x || z >= level_prop->cells_z) {
+	printf_chat("No free spaces found below you");
+	return;
+    } else {
+	if (y> level_prop->cells_y) y = level_prop->cells_y;
+	for(;;) {
+	    y--;
+	    if (y < 1) {
+		printf_chat("No free spaces found below you");
+		return;
+	    }
+	    if (level_blocks[World_Pack(x, y, z)] != Block_Air)
+		break;
+	}
+	for(; y>=0; y--) {
+	    if (y>=1) {
+		uintptr_t index = World_Pack(x, y, z);
+		if (level_blocks[index] == Block_Air &&
+		    level_blocks[World_Pack(x, y-1, z)] == Block_Air)
+		    break;
+	    }
+	}
+	if (y<0) {
+	    printf_chat("No free spaces found below you");
+	    return;
+	}
+    }
+
+    newposn.y = y*32 + 16;
+    send_posn_pkt(-1, &player_posn, newposn);
+    player_posn = newposn;
 }
